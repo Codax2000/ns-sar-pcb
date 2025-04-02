880c8939cb4f658a0ab407e60ac30c2ef7c49e42
Alex Knowlton <aknowlti2000@gmail.com>
2025-04-01 22:37:53 -0700

    ADC working with both modes, now need to refactor into something more readable and useable

--- python/nssar.py
+++ python/nssar.py
@@ -1,237 +1,194 @@
 '''
 Alex Knowlton
-3/24/2025
+4/1/2025
 
-Simulation for a noise-shaping 2nd order SAR ADC with 1st-order DEM
+Class definition for modeling a noise-shaping SAR ADC
+using the Silva-Steensgard architecture, with an
+optional reset
 '''
 
-
+from scipy.signal import iirfilter, windows, lfilter
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import butter, freqz
-import pdb
 
+class NSSAR:
 
-def adc_loop():
-    '''
-    Main simulation loop
-    '''
-    # circuit/testing parameters
-    quantizer_bits = 3
-    sigma = 0.02
-    use_mismatch = False
-    fs = 10**8 # 100 MHz for now
-    prime = 17
-    vdd = 1
-    vss = 0
-    vcm = (vdd - vss) / 2
+    def __init__(self, n_quantizer_bits=3, cap_mismatch_sigma=0.02, \
+                 n_integer_bits=0, n_fractional_bits=0, filter_order=6,
+                 vdd=1.2, vss=0, max_nfft=2**20):
+        '''
+        set class with hardware constraints and initialize register fields
+        with default functions
+        '''
+        self._registers = {
+            'nfft': 2**12,
+            'osr': 32,
+            'fs': 100e6,
+            'incremental_mode': False,
+            'do_dwa': True,
+            'reset_dwa': False,
+            'offset_samples': 1500
+        }
+        normalized_fc = 1 / (2 * self._registers['nfft'])
+        b, a = iirfilter(filter_order, 2 * normalized_fc, \
+                         btype='lowpass', output='ba', ftype='butter')
+        self._n_fractional_bits = n_fractional_bits
+        self._n_integer_bits = n_integer_bits
+        b = self._fp_quantize(b)
+        a = self._fp_quantize(a)
+        self._registers['filter_numerator'] = b
+        self._registers['filter_denominator'] = a
+        self._cp = np.random.normal(1, cap_mismatch_sigma, \
+                                    2 ** n_quantizer_bits)
+        self._cn = np.random.normal(1, cap_mismatch_sigma, \
+                                    2 ** n_quantizer_bits)
+        self._generate_loop_arrays()
+        self._result_memory = np.zeros(max_nfft, dtype=int)
+        self._vrefp = vdd
+        self._vrefn = vss
+    
+    def convert(self, signal_specs):
+        '''
+        Converts a list of dictionaries of the type below and stores them
+        in memory
+        '''
+        self._generate_control_signals()
+        self._generate_input_signals(signal_specs)
+        nfft_derived = self._registers['nfft'] * self._registers['osr']
+        offset = self._registers['osr'] * self._registers['offset_samples']
+        for i in range(offset + nfft_derived):
+            self._calculate_integrator_to_quantizer(i)
+            self._calculate_sample_to_quantizer(i)
+            self._quantize(i)
+            self._update_dwa(i)
+            self._update_analog_integrators(i)
+            self._update_incremental_integrators(i)
+            self._step_iir_filter(i)
+        self._write_data_to_memory()
+    
+    def write_register_value(self, key, value):
+        '''
+        writes a register value if the key is valid and if the value is valid
+        if the key is not valid, throws an UnfoundRegisterError
+        if the value is not valid, throws an IllegalRegisterValueError
+        '''
+        pass
 
-    # control registers
-    osr = 8
-    nfft = 2**11
-    incremental_mode = False
-    use_dwa = False
-    n_offset = 10000
+    def read_register_value(self, key):
+        '''
+        if the register exists, return the value
+        if the register does not exist, throw an UnfoundRegisterError
+        '''
+        pass
 
-    # derived parameters
-    T = 1 / fs
-    nfft_derived = nfft * osr
-    fin = fs * prime / nfft_derived
-    a_in = vdd * 10 ** (-6 / 20)  # input amplitude: -1 dBFS
+    def read_output_data(self):
+        '''
+        Return nfft + n_offset values from device memory as a Pandas Series
+        with time-series indices
+        '''
+        pass
 
-    # calculate filter coefficients
-    normalized_fc = prime / nfft_derived
-    b, a = butter(2, normalized_fc, btype='low', analog=False)
+    def read_conversion_data(self):
+        '''
+        Return osr * (nfft + n_offset) values from the conversion as a Pandas
+        DataFrame with time-series index
+        '''
+        pass
 
-    # calculate mismatched capacitor arrays if using mismatch, all 1 otherwise
-    cp = get_cap_array(quantizer_bits, sigma, use_mismatch)
-    cn = get_cap_array(quantizer_bits, sigma, use_mismatch)
+    def plot_output_fft(self, ax=None):
+        '''
+        Plot the FFT of the output and return a handle of the figure object
+        on which it's plotted. If ax is none, create and set up a new figure
+        and return it. If ax is not none, assume it's already set up and plot
+        on that
+        '''
+        pass
 
-    # time signal
-    t = T * np.arange(n_offset + nfft_derived)
+    def plot_quantizer_fft(self, ax=None):
+        '''
+        Plot the FFT of the output and return a handle of the figure object
+        on which it's plotted. If ax is none, create and set up a new figure
+        and return it. If ax is not none, assume it's already set up and plot
+        on that
+        '''
+        pass
 
-    # input signal
-    U = a_in * np.cos(2 * np.pi * fin * t)
-    # U = U * 0 + 0.8
-    E = np.zeros(t.shape)
+    def plot_output_data(self, n_samples, ax=None):
+        '''
+        plots the output data in the interval (n_offset, n_offset + n_samples)
+        on the given axis and returns a handle to the figure on which it is
+        plotted
+        '''
+        pass
 
-    # analog integrators
-    I1 = np.zeros(t.shape)
-    I2 = np.zeros(t.shape)
+    def get_sndr(self):
+        '''
+        Returns the SNDR of the last conversion. If no conversion has been
+        done yet, return 0
+        '''
+        pass
 
-    # digital integrators
-    D1_continuous = np.zeros(t.shape, dtype=int)
-    D1_incremental = np.zeros(t.shape, dtype=int)
-    D2_incremental = np.zeros(t.shape, dtype=int)
+    def get_sfdr(self):
+        '''
+        Returns the SFDR of the last conversion. If no conversion has been
+        done yet, return 0
+        '''
+        pass
 
-    # SAR quantizer inputs
-    Qin_sample = np.zeros(t.shape)
-    Qin_integ = np.zeros(t.shape)
+    class UnfoundRegisterError(Exception):
+        def __init__(self, register_name):
+            super().__init__(f'Register name \'{register_name}\' not found')
+    
+    class IllegalRegisterValueError(Exception):
+        def __init__(self, key, value):
+            super().__init__(f'Value {value} illegal for register {key}')
 
-    # boolean signals, useful for logic development later
-    reset = np.zeros(t.shape, dtype=bool)
-    sample_output = np.zeros(t.shape, dtype=bool)
-    dwa_pointer = np.zeros(t.shape, dtype=int)  # don't use this yet
+    def _fp_quantize(self, value):
+        pass
 
-    # quantizer output
-    V = np.zeros(t.shape)
-    dac_output = np.zeros(t.shape)
+    def _generate_loop_arrays(self):
+        pass
 
-    # differential input signals
-    vinp = np.zeros(t.shape)
-    vinn = np.zeros(t.shape)
-    vintp = np.zeros(t.shape)
-    vintn = np.zeros(t.shape)
+    def _generate_control_signals(self):
+        pass
 
-    # IADC loop
-    for i in range(t.shape[0]):
-        print(f'Cycle {i}')
-        # sample input
-        Qin_sample[i] = U[i]
+    def _generate_input_signals(self, data):
+        pass
 
-        # calculate integrator input to quantizer
-        if (i == 0):
-            Qin_integ[i] = 0
-        else:
-            Qin_integ[i] = 2 * I1[i - 1] + I2[i - 1]
-        
-        # convert to differential voltages
-        vintp[i] = vcm + Qin_integ[i] / 2
-        vintn[i] = vcm - Qin_integ[i] / 2
-        vinp[i] = vcm + Qin_sample[i] / 2
-        vinn[i] = vcm - Qin_sample[i] / 2
-        
-        # quantize with DWA
-        V[i], dac_output[i] = sar_quantize(vinp[i], vinn[i], vintp[i], vintn[i], cp, cn, 0)
-        E[i] = dac_output[i]
-        # pdb.set_trace()
-        # V[i] = quantize(vinp[i], vinn[i], vintp[i], vintn[i])
-        # E[i] = U[i] - V[i]
-        # update analog integrators
-        if (i == 0):
-            I1[i] = E[i]
-            I2[i] = 0
-        else:
-            I1[i] = E[i] + I1[i - 1]
-            I2[i] = I1[i - 1] + I2[i - 1]
+    def _calculate_integrator_to_quantizer(self, i):
+        pass
 
-    plt.figure()
+    def _calculate_sample_to_quantizer(self, i):
+        pass
 
-    fft_pow = np.fft.fft(V[-nfft_derived:])
-    fft_pow = np.conj(fft_pow) * fft_pow
-    fft_pow_no_dc = fft_pow[1:(1+nfft_derived // 2)]
-    fft_pow_no_dc_db = 10 * np.log10(fft_pow_no_dc)
-    plt.semilogx(fft_pow_no_dc_db)
+    def _quantize(self, i):
+        pass
 
+    def _update_dwa(self, i):
+        pass
 
-def get_cap_array(n_bits, sigma, use_mismatch):
-    '''
-    Returns an array of 2^n_bits capacitor values drawn from a
-    normal distribution with a standard deviation of sigma
-    '''
-    if use_mismatch:
-        return np.random.normal(1, sigma, 2**n_bits)
-    else:
-        return np.ones(2**n_bits)
+    def _update_analog_integrators(self, i):
+        pass
 
+    def _update_incremental_integrators(self, i):
+        pass
 
-def quantize(vip, vin, vintp, vintn):
-    '''
-    single-bit quantizer function
-    '''
-    U = vip - vin
-    W = vintp - vintn
-    V = 1 if (U + W) > 0 else -1
-    return V
+    def _step_iir_filter(self, i):
+        pass
 
+    def _write_data_to_memory(self):
+        pass
 
-def sar_quantize(vip, vin, vintp, vintn, cp, cn, dwa_pointer=0):
-    '''
-    SAR quantizer function using two integrators and cap update functions
-    also uses positive and negative reference voltages for more accurate numbers
-    additionally, user can set the DWA pointer to shift the update voltages
-    for DEM
-    '''
-    n_bits = int(np.log2(len(cp)))
-    vrefp = 1
-    vrefn = 0
+    def _get_fft_frequencies(self, use_quantizer_data=False):
+        '''
+        Return frequencies of fft as frequency, not normalized to fs. If using
+        quantizer, go up to fs / 2, else only go up to fs / (2 * osr)
+        '''
+        pass
 
-    # roll capacitor values if using DWA
-    cp = np.roll(cp, -dwa_pointer)
-    cn = np.roll(cn, -dwa_pointer)
-
-    # residue voltage for all conversions, at all points in conversion
-    vresp = np.zeros(n_bits + 1)
-    vresn = np.zeros(n_bits + 1)
-    bits = np.zeros(n_bits, dtype=int)
-
-    vcm = (vrefp + vrefn) / 2
-
-    cap_values = np.zeros(2**n_bits) + vcm
-    vresp[0] = vrefp - vip
-    vresn[0] = vrefp - vin
-    for i in range(n_bits):
-        filt = vintp + vresn[i] >= vintn + vresp[i]
-        bits[i] = 1 * filt
-        cap_values = update_cap_values(cap_values, i, filt)
-        inverted_cap_values = 1 - cap_values
-        qp = np.sum(cp * (cap_values)) + np.sum(cp) * (vresp[0] - vcm)
-        qn = np.sum(cn * (inverted_cap_values)) + np.sum(cn) * (vresn[0] - vcm)
-        vresp[i + 1] = qp / np.sum(cp)
-        vresn[i + 1] = qn / np.sum(cn)
-    bits_calc = n_bits - 1
-    positive_bits = bits_calc - np.where(bits == 1)[0]
-    return np.sum(np.pow(2, positive_bits)), vresn[n_bits] - vresp[n_bits]
-
-
-def update_cap_values(cap_values, i, bit):
-    '''
-    Update capacitor voltages to either 1 or 0 depending on the SAR conversion
-    '''
-    # calculate slice to update
-    n_bits = int(np.log2(cap_values.shape[0]))
-    n_update_bits = 2**(n_bits - (i + 1))
-    update_slice = np.where(cap_values == 0.5)[0]
-    if bit:
-        cap_values[update_slice[:n_update_bits]] = 1
-    else:
-        cap_values[update_slice[-(n_update_bits + 1):-1]] = 0
-    return cap_values
-
-
-def plot_sar_conversion():
-    vin = np.arange(-1, 1.001, 0.001)
-    vres = np.zeros(vin.shape)
-    dout = np.zeros(vin.shape)
-    vcm = 0.5
-    caps = np.ones(16)
-    for i in range(len(vin)):
-        vp = vin[i] / 2 + vcm
-        vn = -vin[i] / 2 + vcm
-        d, v = sar_quantize(vp, vn, 0, 0, caps, caps, 0)
-        dout[i] = d
-        vres[i] = v
-    plt.figure()
-    plt.subplot(2, 1, 1)
-    plt.plot(vin, dout)
-    plt.grid()
-    plt.title('SAR Quantization Across Voltage')
-    plt.ylabel('Digital Output')
-    plt.subplot(2, 1, 2)
-    plt.plot(vin, vres)
-    plt.xlabel('$V_{in}$')
-    plt.ylabel('$V_{res}$')
-    plt.grid()
-
-
-def main():
-    '''
-    Main loop
-    '''
-    # plot_sar_conversion()
-    adc_loop()
-    plt.show()
-
-
-if __name__ == '__main__':
-    main()
+    def _get_windowed_fft_data(self, use_quantizer_data=False):
+        '''
+        Return windowed fft data as power normalized to the fundamental tone(s)
+        if not use_quantizer_data, use the output data
+        '''
+        pass
---
+++ python/nssar_prototype.py
@@ -0,0 +1,270 @@
+'''
+Alex Knowlton
+3/24/2025
+
+Simulation for a noise-shaping 2nd order SAR ADC with 1st-order DEM
+'''
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import iirfilter, lfilter, windows
+from scipy.fft import fft
+import pdb
+
+
+def adc_loop():
+    '''
+    Main simulation loop
+    '''
+    # circuit/testing parameters
+    quantizer_bits = 3
+    sigma = 0.03
+    use_mismatch = False
+    fs = 10**8 # 100 MHz for now
+    prime = 97
+    vdd = 1
+    vss = 0
+    vcm = (vdd - vss) / 2
+
+    # control registers
+    osr = 16
+    nfft = 2**12
+    incremental_mode = False
+    use_dwa = True
+    reset_dwa = False
+    n_offset = 10000 if not incremental_mode else 0
+
+    # derived parameters
+    T = 1 / fs
+    nfft_derived = nfft * osr
+    fin = fs * prime / nfft_derived
+    a_in = vdd * 10 ** (-6 / 20)  # input amplitude: -1 dBFS
+
+    # calculate filter coefficients
+    normalized_fc = 1 / (2 * osr)
+    b, a = iirfilter(6, normalized_fc, btype='lowpass', output='ba', ftype='butter')
+
+    # calculate mismatched capacitor arrays if using mismatch, all 1 otherwise
+    cp = get_cap_array(quantizer_bits, sigma, use_mismatch)
+    cn = get_cap_array(quantizer_bits, sigma, use_mismatch)
+
+    # time signal
+    t = T * np.arange(n_offset + nfft_derived)
+
+    # input signal
+    U = a_in * np.cos(2 * np.pi * fin * t)
+    # U = U * 0 + 0.8
+    E = np.zeros(t.shape)
+
+    # analog integrators
+    I1 = np.zeros(t.shape)
+    I2 = np.zeros(t.shape)
+
+    # digital integrators
+    D1_continuous = np.zeros(t.shape, dtype=int)
+    D1_incremental = np.zeros(t.shape, dtype=int)
+    D2_incremental = np.zeros(t.shape, dtype=int)
+
+    # SAR quantizer inputs
+    Qin_sample = np.zeros(t.shape)
+    Qin_integ = np.zeros(t.shape)
+
+    # boolean signals, useful for logic development later
+    reset = np.zeros(t.shape, dtype=bool)
+    sample_output = np.zeros(t.shape, dtype=bool)
+    dwa_pointer = np.zeros(t.shape, dtype=int)  # don't use this yet
+
+    # quantizer output
+    V = np.zeros(t.shape)
+    dac_output = np.zeros(t.shape)
+
+    # differential input signals
+    vinp = np.zeros(t.shape)
+    vinn = np.zeros(t.shape)
+    vintp = np.zeros(t.shape)
+    vintn = np.zeros(t.shape)
+    # IADC loop
+    for i in range(t.shape[0]):
+        # sample input
+        reset[i] = ((i % osr) == 0 and incremental_mode) or (i == 0)
+        sample_output[i] = (i % osr) == (osr - 1)
+        Qin_sample[i] = U[i]
+
+        # calculate integrator input to quantizer
+        if (reset[i] or (i == 0)):
+            Qin_integ[i] = 0
+        else:
+            Qin_integ[i] = 2 * I1[i - 1] + I2[i - 1]
+        
+        # convert to differential voltages
+        vintp[i] = vcm + Qin_integ[i] / 2 
+        vintn[i] = vcm - Qin_integ[i] / 2
+        vinp[i] = vcm + Qin_sample[i] / 2
+        vinn[i] = vcm - Qin_sample[i] / 2
+        
+        # quantize with DWA
+        if ((i == 0) or (not use_dwa) or (reset[i] and use_dwa and reset_dwa)):
+            pointer = 0
+        else:
+            pointer = dwa_pointer[i - 1]
+        V[i], dac_output[i] = sar_quantize(vinp[i], vinn[i], vintp[i], vintn[i], cp, cn, pointer)
+        E[i] = dac_output[i]
+        pointer += V[i]
+        dwa_pointer[i] = pointer % (2 ** quantizer_bits)
+        # pdb.set_trace()
+        # V[i] = quantize(vinp[i], vinn[i], vintp[i], vintn[i])
+        # E[i] = U[i] - V[i]
+        # update analog integrators
+        if (reset[i] or (i == 0)):
+            I1[i] = E[i]
+            I2[i] = 0
+        else:
+            I1[i] = E[i] + I1[i - 1]
+            I2[i] = I1[i - 1] + I2[i - 1]
+        
+        # update digital integrators
+        if (reset[i] or (i == 0)):
+            D1_incremental[i] = V[i]
+            D2_incremental[i] = D1_incremental[i]
+        else:
+            D1_incremental[i] = V[i] + D1_incremental[i - 1]
+            D2_incremental[i] = D1_incremental[i] + D2_incremental[i - 1]
+
+    incremental_out = D2_incremental[sample_output] * 1 / (2 * osr * (osr + 1)) - ((2 ** quantizer_bits - 1) / (2 ** quantizer_bits))
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.plot(incremental_out[-nfft:], linewidth=0.5)
+    plt.subplot(2, 1, 2)
+    fft_vals = fft(incremental_out[-nfft:])
+    fft_pow = np.real(fft_vals * np.conj(fft_vals))
+    fft_pow_trimmed = fft_pow[1:1 + (nfft // 2)]
+    fft_pow_trimmed[-1] /= 2
+    fft_pow_db = 10 * np.log10(fft_pow_trimmed)
+    freq = np.arange(1, (nfft // 2) + 1) * fs / nfft
+    plt.plot(freq, fft_pow_db, linewidth=0.5)
+    plt.figure()
+    V = V - (2 ** quantizer_bits - 1) / 2
+    vfilt = lfilter(b, a, V[-nfft_derived:], axis=0)
+    continuous_out = vfilt[sample_output[-nfft_derived:]]
+    plt.subplot(2, 1, 1)
+    fft_window = windows.hann(nfft_derived)
+    fft_data = fft_window * vfilt
+    plt.loglog(np.abs(fft(fft_data)[:32768]))
+    plt.subplot(2, 1, 2)
+    fft_window = windows.blackman(nfft)
+    fft_data = fft_window * continuous_out
+    plt.loglog(np.abs(fft(fft_data)[:(nfft//2)]))
+
+
+def get_cap_array(n_bits, sigma, use_mismatch):
+    '''
+    Returns an array of 2^n_bits capacitor values drawn from a
+    normal distribution with a standard deviation of sigma
+    '''
+    if use_mismatch:
+        return np.random.normal(1, sigma, 2**n_bits)
+    else:
+        return np.ones(2**n_bits)
+
+
+def quantize(vip, vin, vintp, vintn):
+    '''
+    single-bit quantizer function
+    '''
+    U = vip - vin
+    W = vintp - vintn
+    V = 1 if (U + W) > 0 else -1
+    return V
+
+
+def sar_quantize(vip, vin, vintp, vintn, cp, cn, dwa_pointer=0):
+    '''
+    SAR quantizer function using two integrators and cap update functions
+    also uses positive and negative reference voltages for more accurate numbers
+    additionally, user can set the DWA pointer to shift the update voltages
+    for DEM
+    '''
+    n_bits = int(np.log2(len(cp)))
+    vrefp = 1
+    vrefn = 0
+
+    # roll capacitor values if using DWA
+    cp = np.roll(cp, -dwa_pointer)
+    cn = np.roll(cn, -dwa_pointer)
+
+    # residue voltage for all conversions, at all points in conversion
+    vresp = np.zeros(n_bits + 1)
+    vresn = np.zeros(n_bits + 1)
+    bits = np.zeros(n_bits, dtype=int)
+
+    vcm = (vrefp + vrefn) / 2
+
+    cap_values = np.zeros(2**n_bits) + vcm
+    vresp[0] = vrefp - vip
+    vresn[0] = vrefp - vin
+    for i in range(n_bits):
+        filt = vintp + vresn[i] >= vintn + vresp[i]
+        bits[i] = 1 * filt
+        cap_values = update_cap_values(cap_values, i, filt)
+        inverted_cap_values = 1 - cap_values
+        qp = np.sum(cp * (cap_values)) + np.sum(cp) * (vresp[0] - vcm)
+        qn = np.sum(cn * (inverted_cap_values)) + np.sum(cn) * (vresn[0] - vcm)
+        vresp[i + 1] = qp / np.sum(cp)
+        vresn[i + 1] = qn / np.sum(cn)
+    bits_calc = n_bits - 1
+    positive_bits = bits_calc - np.where(bits == 1)[0]
+    return np.sum(np.pow(2, positive_bits)), vresn[n_bits] - vresp[n_bits]
+
+
+def update_cap_values(cap_values, i, bit):
+    '''
+    Update capacitor voltages to either 1 or 0 depending on the SAR conversion
+    '''
+    # calculate slice to update
+    n_bits = int(np.log2(cap_values.shape[0]))
+    n_update_bits = 2**(n_bits - (i + 1))
+    update_slice = np.where(cap_values == 0.5)[0]
+    if bit:
+        cap_values[update_slice[:n_update_bits]] = 1
+    else:
+        cap_values[update_slice[-(n_update_bits + 1):-1]] = 0
+    return cap_values
+
+
+def plot_sar_conversion():
+    vin = np.arange(-1, 1.001, 0.001)
+    vres = np.zeros(vin.shape)
+    dout = np.zeros(vin.shape)
+    vcm = 0.5
+    caps = np.ones(16)
+    for i in range(len(vin)):
+        vp = vin[i] / 2 + vcm
+        vn = -vin[i] / 2 + vcm
+        d, v = sar_quantize(vp, vn, 0, 0, caps, caps, 0)
+        dout[i] = d
+        vres[i] = v
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.plot(vin, dout)
+    plt.grid()
+    plt.title('SAR Quantization Across Voltage')
+    plt.ylabel('Digital Output')
+    plt.subplot(2, 1, 2)
+    plt.plot(vin, vres)
+    plt.xlabel('$V_{in}$')
+    plt.ylabel('$V_{res}$')
+    plt.grid()
+
+
+def main():
+    '''
+    Main loop
+    '''
+    # plot_sar_conversion()
+    adc_loop()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
