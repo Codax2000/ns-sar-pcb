0150f2a81e738b5e2449c73849b9dace94a34a19
Alexander Knowlton <aknowlti2000@gmail.com>
2026-02-15 00:02:31 -0800

    synchronizer exporter working with the correct declaration, now just need to hook up the right interfaces

--- scripts/synchronizer_exporter.py
+++ scripts/synchronizer_exporter.py
@@ -9,6 +9,21 @@ from systemrdl.node import FieldNode
 # Define a listener that will print out the register model hierarchy
 class MyModelPrintingListener(RDLListener):
 
+    def enter_Addrmap(self, node):
+        name = node.inst_name
+
+        n_sync_stages = 2
+        print(f'module #(')
+        print(f'\tparameter N_SYNC_STAGES={n_sync_stages},')
+        print(f'\tparameter SRC_INPUT_REG={0}')
+        print(f') {name}_reg_sync (')
+        print(f'\t{name}__in_t hwif_in_sysclk')
+        print(f'\t{name}__in_t hwif_in_ifclk')
+        print(f'\t{name}__out_t hwif_out_sysclk')
+        print(f'\t{name}__out_t hwif_out_ifclk')
+        print('\tinput logic sysclk,')
+        print('\tinput logic ifclk\n);')
+
     def enter_Field(self, node):
         # Print some stuff about the field
         sw_access_str = f"hw={node.get_property('hw').name}"
@@ -20,12 +35,17 @@ class MyModelPrintingListener(RDLListener):
         else:
             hwif_name = 'hwif_out'
             hwif_value = 'value'
-        hwif_path = f'{hwif_name}.{hwif_path}.{hwif_value}'
+        hwif_in_path = f'\t{hwif_name}.{hwif_path}.{hwif_value}'
+        hwif_out_path = f'\t{hwif_name}.{hwif_path}.{hwif_value}'
 
         # unnecessary with synchronizer
         # if node.width > 1:
         #     hwif_path = f'{hwif_path}[{node.width - 1}:0]'
-
+        print('sync_nstage #(')
+        print('\t.N_SYNC_STAGES(N_SYNC_STAGES),')
+        print('\t.SRC_INPUT_REG(SRC_INPUT_REG),')
+        print(f"\t.N_BITS({node.width})")
+        print(f") {'.'.join(hwif_path.split('.')[1:3]).replace('.', '_')}_sync (")
         print(sw_access_str, hwif_path)
 
 rdlc = RDLCompiler()
